d38b0ed9ba4a62b638025124d51061a7983a2008
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-07-20 14:29:28 -0300

    Add math.js with sum and multiplication

---
+++ math.js
@@ -0,0 +1,26 @@
+var sum = function() {
+  var result = 0;
+  if (arguments.length) {
+    Array.prototype.slice.call(arguments)
+    .forEach(function(number, index) {
+      if (typeof number !== 'number') {
+        throw new TypeError('Expected a number :/');
+      } else {
+        result += number;
+      }
+    });
+  }
+
+  return result;
+};
+
+var multiplication = function() {
+  return Array.prototype.slice.call(arguments)
+  .reduce(function(previous, current) {
+    var result = 0;
+    for (var i = 0; i < previous; i++) {
+      result += sum.call(null, current);
+    }
+    return result;
+  });
+};
