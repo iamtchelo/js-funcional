3789673007873f8e739eaef4e27047f62af84d32
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-07-21 10:03:12 -0300

    Validate type number on multiplication()

--- math.js
+++ math.js
@@ -17,6 +17,10 @@ var sum = function() {
 var multiplication = function() {
   return Array.prototype.slice.call(arguments)
   .reduce(function(previous, current) {
+    if (typeof previous !== 'number' || typeof current !== 'number') {
+      throw new TypeError('Expected a number in previous and current :/');
+    }
+
     var result = 0;
     for (var i = 0; i < previous; i++) {
       result += sum.call(null, current);
