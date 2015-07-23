eb99cfe53eee0a5fe4328e807fdb05d8b3974d41
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-07-22 22:46:57 -0300

    Add Calculator obj and create temporary escope

--- math.js
+++ math.js
@@ -1,31 +1,37 @@
-var sum = function() {
-  var result = 0;
-  if (arguments.length) {
-    Array.prototype.forEach.call(arguments, function(number) {
-      if (typeof number !== 'number') {
-        throw new TypeError('Expected a number :/');
-      } else {
-        result += number;
-      }
-    });
-  }
+(function(root) {
+  var Calculator = root.Calculator || {};
 
-  return result;
-};
+  Calculator.sum = function() {
+    var result = 0;
+    if (arguments.length) {
+      Array.prototype.forEach.call(arguments, function(number) {
+        if (typeof number !== 'number') {
+          throw new TypeError('Expected a number :/');
+        } else {
+          result += number;
+        }
+      });
+    }
+    return result;
+  };
 
-var multiplication = function() {
-  if (arguments.length) {
-    return Array.prototype.reduce.call(arguments, function(previous, current) {
-      if (typeof previous !== 'number' || typeof current !== 'number') {
-        throw new TypeError('Expected a number in previous and current :/');
-      }
+  Calculator.multiplication = function() {
+    var _self = this;
+    if (arguments.length) {
+      return Array.prototype.reduce.call(arguments, function(previous, current) {
+        if (typeof previous !== 'number' || typeof current !== 'number') {
+          throw new TypeError('Expected a number in previous and current :/');
+        }
 
-      var result = 0;
-      for (var i = 0; i < previous; i++) {
-        result += sum.call(null, current);
-      }
+        var result = 0;
+        for (var i = 0; i < previous; i++) {
+          result += _self.sum.call(null, current);
+        }
+        return result;
+      });
+    }
+  };
 
-      return result;
-    });
-  }
-};
+  // exports
+  root.Calculator = Calculator;
+}(window));
